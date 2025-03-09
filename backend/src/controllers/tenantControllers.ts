59b4e85d649adecb6a8367589fe9998cf2814096
0QKub0 <qkubplay@gmail.com>
2025-03-09 15:29:08 +0100

    adding and removing favorite property for tenants

--- backend/src/controllers/tenantControllers.ts
+++ backend/src/controllers/tenantControllers.ts
@@ -78,7 +78,7 @@ export const getCurrentResidences = async (
   try {
     const { cognitoId } = req.params;
     const properties = await prisma.property.findMany({
-      where: { tenants: {some:{cognitoId}} },
+      where: { tenants: { some: { cognitoId } } },
       include: {
         location: true,
       },
@@ -110,9 +110,71 @@ export const getCurrentResidences = async (
 
     res.json(residencesWithFormattedLocation);
   } catch (error: any) {
-    res
-      .status(500)
-      .json({ message: `Error retrieving tenant residences: ${error.message}` });
+    res.status(500).json({
+      message: `Error retrieving tenant residences: ${error.message}`,
+    });
   }
 };
+export const addFavoriteProperty = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { cognitoId, propertyId } = req.params;
+    const tenant = await prisma.tenant.findUnique({
+      where: { cognitoId },
+      include: { favorites: true },
+    });
 
+    if (!tenant) {
+      res.status(404).json({ message: "Tenant not found" });
+      return;
+    }
+
+    const propertyIdNumber = Number(propertyId);
+    const existingFavorites = tenant?.favorites || [];
+
+    if (!existingFavorites.some((fav) => fav.id === propertyIdNumber)) {
+      const updatedTenant = await prisma.tenant.update({
+        where: { cognitoId },
+        data: {
+          favorites: {
+            connect: { id: propertyIdNumber },
+          },
+        },
+        include: { favorites: true },
+      });
+      res.json(updatedTenant);
+    } else {
+      res.status(409).json({ message: "Property already added as favorite" });
+    }
+  } catch (error: any) {
+    res.status(500).json({
+      message: `Error adding favorite property: ${error.message}`,
+    });
+  }
+};
+export const removeFavoriteProperty = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { cognitoId, propertyId } = req.params;
+    const propertyIdNumber = Number(propertyId);
+
+    const updatedTenant = await prisma.tenant.update({
+      where: { cognitoId },
+      data: {
+        favorites: {
+          disconnect: { id: propertyIdNumber },
+        },
+      },
+      include: { favorites: true },
+    });
+    res.json(updatedTenant);
+  } catch (error: any) {
+    res.status(500).json({
+      message: `Error removing favorite property: ${error.message}`,
+    });
+  }
+};
--- backend/src/routes/tenantRoutes.ts
+++ backend/src/routes/tenantRoutes.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import {
     getTenant, createTenant, updateTenant,
-    getCurrentResidences
+    getCurrentResidences,
+    addFavoriteProperty,
+    removeFavoriteProperty
 } from "../controllers/tenantControllers";
 const router = express.Router();
 router.get("/:cognitoId",getTenant);
 router.post("/",createTenant);
 router.put("/:cognitoId", updateTenant);
 router.get("/:cognitoId/current-residences",getCurrentResidences);
+router.post("/:cognitoId/favorite/:propertyId", addFavoriteProperty);
+router.delete("/:cognitoId/favorite/:propertyId", removeFavoriteProperty);
+
 
 export default router;
